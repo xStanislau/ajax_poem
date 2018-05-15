c6ddd0c192b56e1c1caec76f57871a196c6da744
Stanislau <grench333@gmail.com>
2018-05-15 17:54:53 +0300

    change index property in ui-server config from ajax-start.html to index.html

--- ui-server.js
+++ ui-server.js
@@ -5,6 +5,6 @@ var bs = require("browser-sync").create();
 // Now init the Browsersync server
 bs.init({
     server: "./",
-    index: './ajax-start.html',
+    index: './index.html',
     files: ['*.html','css/*.css', 'js/*.js']
 });
